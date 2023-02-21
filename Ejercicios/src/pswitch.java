1daddc8c4a13bb4f6c6165430fd2828da6fe96c4
julip <juliperalta1306@gmail.com>
2023-02-21 08:09:50 -0500

    Se agregaron las clases pswitch y calculadora

---
+++ Ejercicios/src/pswitch.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class pswitch {
+    public static void main(String[] args) throws Exception {
+       Scanner lectura=new Scanner (System.in);
+       String vocal;
+       System.out.println("digite una vocal");
+       vocal=lectura.nextLine();
+       switch (vocal){
+        case "A": 
+        System.out.println("Usted eigio la vocal Aa");
+        break;
+        case "E":
+        System.out.println("Usted eigio la vocal Ee");
+        break;
+        case "I":
+        System.out.println("Usted eligio la vocal Ii");
+        break;
+        case "O":
+        System.out.println("Usted eligio la vocal Oo");
+        break;
+        case "U":
+        System.out.println("Usted eligio la vocal Uu");
+        break;
+        case "a": 
+        System.out.println("Usted eigio la vocal Aa");
+        break;
+        case "e": 
+        System.out.println("Usted eigio la vocal Ee");
+        break;
+        case "i": 
+        System.out.println("Usted eigio la vocal Ii");
+        break;
+        case "o": 
+        System.out.println("Usted eigio la vocal Oo");
+        break;
+        case "u": 
+        System.out.println("Usted eigio la vocal Uu");
+        break;
+        default:
+        System.out.println("usted no digito una vocal reintente");
+       }
+       
+        lectura.close();
+    }
+}
