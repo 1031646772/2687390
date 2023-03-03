cb20e7976e5b06a97ebbd02b85018f383358b959
1031646772 <juliperalta1306@gmail.com>
2023-03-03 12:09:17 -0500

    Se modifico la clase Challenge2

--- Ejercicios/src/estructuradatos/Challenge2.java
+++ Ejercicios/src/estructuradatos/Challenge2.java
@@ -4,41 +4,33 @@ public class Challenge2 {
     static Scanner lectura= new Scanner (System.in);
     static Scanner entrada= new Scanner (System.in);
     public static void main(String[] args) {
-        int competitors,mayor;
+        int competitors,menor,pos=0;
         System.out.println("Digite la catidad de competidores");
         competitors=entrada.nextInt();
         String []datescompetence=new String [competitors];
         int [] timecompetence=new int [competitors];
+        String  competidor=datescompetence[pos];
         
         for (int p=0;p<datescompetence.length;p++){
             System.out.println("Digite el nombre del competidor");
             datescompetence[p]=lectura.nextLine();
             System.out.println("Digite el tiempo que registro el competidor en segundos");
             timecompetence[p]=entrada.nextInt();
-            for (String i: datescompetence) {
-                for (int j : timecompetence) {
-                System.out.print("El competidor "+i+" registro un tiempo de:");
-                 System.out.println(j);
-                 break;
-                }
-                }
             }
-            mayor=timecompetence[0];
             for (int p=0;p<datescompetence.length;p++){
-            if (timecompetence[p]>mayor){
-                mayor=timecompetence[p];
-                for( int u=0;u<timecompetence.length;u++){
-                    if (timecompetence[u]==mayor){
-                        int pos=u;
-                        String conversion=String.valueOf(mayor);
-                        String competidor=datescompetence[pos];
-                        System.out.println(competidor);
-                        System.out.println("El competidor que mas tiempo hizo fue "+competidor+"que tiene un tiempo de:"+conversion);
-                    } 
-                    break;
-                 }   
+                System.out.println("El competidor "+datescompetence[p]+" registro un tiempo de:"+timecompetence[p]);   
             }
-        
-        }
-    }}
+                menor=timecompetence[0];
+                for (int p=0;p<timecompetence.length;p++){
+                    if (timecompetence[p]<menor){
+                        menor=timecompetence[p];
+                            if (timecompetence[p]==menor){
+                                pos=p;
+                                competidor=datescompetence[pos];
+                            } 
+                    }
+                
+                }
+                System.out.println("El competidor que menor tiempo hizo fue "+competidor+" que tiene un tiempo de:"+menor);
+            }}
         
